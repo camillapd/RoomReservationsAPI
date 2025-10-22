c8d13fa06d3d86618aab210b55c917a2ddb9b3f6
Camilla <42720869+camillapd@users.noreply.github.com>
2025-10-22 08:19:06 -0300

    Update exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -18,10 +18,11 @@ url = "http://127.0.0.1:5000/reservations/2"
 data = {"reservation_date": "2021-10-21",
         "start_hour": "14:00", 
         "end_hour": "16:00"}
-response = requests.post(url, json=data)
+response = requests.put(url, json=data)
 print(response.status_code)
 
 # DELETE reuniões
 url = "http://127.0.0.1:5000/reservations/2"  
 response = requests.delete(url)
+
 print(response.status_code)
