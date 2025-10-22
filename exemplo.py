b153f0c031529fb98ce42efa7e5e7a6c2946d944
Camilla <42720869+camillapd@users.noreply.github.com>
2025-10-22 08:26:23 -0300

    Update exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -4,14 +4,16 @@ import requests
 url = "http://127.0.0.1:5000/meetingrooms"
 data = {"name": "Sala 201"}
 response = requests.post(url, json=data)
-print(response.status_code)
+print(response.status_code,response.json())
+print()
 
 # POST reuniões
 url = "http://127.0.0.1:5000/reservations"
 data = {"room_name": "Sala 201", "reservation_date": "2021-10-21",
         "start_hour": "11:00", "end_hour": "13:00"}
 response = requests.post(url, json=data)
-print(response.status_code)
+print(response.status_code,response.json())
+print()
 
 # PUT reuniões
 url = "http://127.0.0.1:5000/reservations/2"
@@ -19,10 +21,23 @@ data = {"reservation_date": "2021-10-21",
         "start_hour": "14:00", 
         "end_hour": "16:00"}
 response = requests.put(url, json=data)
-print(response.status_code)
+print(response.status_code,response.json())
+print()
 
 # DELETE reuniões
 url = "http://127.0.0.1:5000/reservations/2"  
 response = requests.delete(url)
-
 print(response.status_code)
+print()
+
+# GET salas
+url = "http://127.0.0.1:5000/meetingrooms/"  
+response = requests.get(url)
+print(response.json())
+print()
+
+# GET reuniões
+url = "http://127.0.0.1:5000/reservations/"  
+response = requests.get(url)
+print(response.json())
+print()
